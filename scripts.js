9f896ada0d81d0c62a4abfa19af7604921ea5254
ALEXANDRA ARGUIMBAU <AARGUIMBAU@ALEXANDRAs-MacBook-Air.local>
2016-03-25 17:29:51 -0400

    number guessing game

--- scripts.js
+++ scripts.js
@@ -6,50 +6,54 @@ console.log('loaded');
 
 var guessNumberGame = {};
 
-//create method for ansewer
-guessNumberGame.init = function($guessForm, $promptArea){
-  //init = initialize
-  var possibleAnswerMax = 100;
-  this.answer = Math.floor(Math.random()* possibleAnswerMax);
-  this.$guessForm = $guessForm;
-  this.$promptArea = $promptArea;
-  this.guessFormHandler();
-  //.Math floor cuts off decimal
-};
+
 guessNumberGame.makeGuess = function(guess){
   if(guess == this.answer){
-    this.winGame();
-  }else{
-    this.tryAgain();
+  this.winGame();
+}else{
+  this.promptUser(guess);
+}
+};
+guessNumberGame.promptUser = function(guess){
+  if(guess > this.answer){
+    var message = "You guessed " + guess + ". Guess Lower! ";
+    this.$promptArea.text(message);
+  } else {
+    var message = "You guessed " + guess + ". Guess Higher! ";
+    this.$promptArea.text(message);
   }
 };
-guessNumberGame.winGame = function(){
-  this.$promptArea.text('You Win!');
 
-}
-guessNumberGame.tryAgain = function(){
-  this.$promptArea.text('Guess Again!');
-}
+guessNumberGame.winGame = function(){
+  this.$promptArea.text('YOU WIN!');
+};
 
 guessNumberGame.guessFormHandler = function(){
   var scope = this;
     this.$guessForm.on('submit', function(e){
       e.preventDefault();
-      var guessInput = $(this).find('input')
+      var guessInput = $(this).find('input');
       var guess = guessInput.val();
       scope.makeGuess(guess);
 
-    })
+    });
+};
+guessNumberGame.init = function($guessForm, $promptArea){
+  //init = initialize
+  var possibleAnswerMax = 100;
+  this.answer = Math.floor(Math.random()*possibleAnswerMax);
+  this.$guessForm = $guessForm;
+  this.$promptArea = $promptArea;
+  this.guessFormHandler();
+  //.Math floor cuts off decimal
 };
 
-//make guess function
 
 
 //********
 //using
 $(function(){
-  var $guessForm = $('#guess-handler')
-  var $promptArea = $('#user-prompt')
+  var $guessForm = $('#guess-handler');
+  var $promptArea = $('#user-prompt');
   guessNumberGame.init($guessForm, $promptArea);
-
 });
